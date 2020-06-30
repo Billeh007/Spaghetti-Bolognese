9ba39b48ee92bf314bdd0e500aa8c26d617e00ed
Billeh007 <thisisnotbilly@hotmail.com>
2020-06-30 03:14:01 -0500

    Nightly backup

--- src/main/java/com/Billeh007/items/ModBowl.java
+++ src/main/java/com/Billeh007/items/ModBowl.java
@@ -1,5 +1,14 @@
 package com.Billeh007.items;
 
-public class ModBowl {
+import net.minecraft.item.ItemSoup;
 
+public class ModBowl extends ItemSoup
+{
+
+	public ModBowl(int p_i45330_1_)
+	{
+		super(p_i45330_1_);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
